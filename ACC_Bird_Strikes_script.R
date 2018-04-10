c979da18691aa70fbe61c18ac441d8439b466212
casey conrad <conradcd@g.cofc.edu>
2018-04-10 01:42:49 -0400

    still good

--- ACC_Bird_Strikes_script.R
+++ ACC_Bird_Strikes_script.R
@@ -7,6 +7,46 @@ library(RCurl)
 require(RCurl)
 full_set = read.csv(text = getURL("https://raw.githubusercontent.com/conradcd/ACC_Bird_Strikes/master/1991-2017_match.csv"), header = T)
 
+# This CSV file is a combination of strike data from 2013-2017
+# pulled from an online database, and older data from 1991-2013 
+# exported to CSV from a Microsoft Access file. The older data 
+# has less info, so columns were deleted from the newer set to match.
+
+# Common species name is missing from the older data, so first...
+# (despite countless attempts, this simple task eluded me)
+
+# First, lets quickly examine which species are most prevalent
+full_sp = full_set$Species_Abv
+abv_freq = table(full_sp)
+sort(abv_freq)
+pie(abv_freq)
+
+# Since I'm having trouble with the common name for all abbreviations,
+# what are the names of the top ten? 
+# Common name for top 10
+for(i in seq(1, nrow(full_set)))  {
+  if(full_set$Species_Abv[i] == "RTHA")
+    full_set$Common.Species.Name[i] = "Red-Tailed Hawk"
+  else if(full_set$Species_Abv[i] == "BDOW")
+    full_set$Common.Species.Name[i] = "Barred Owl"
+  else if(full_set$Species_Abv[i] == "EASO")
+    full_set$Common.Species.Name[i] = "Eastern Screech Owl"
+  else if(full_set$Species_Abv[i] == "GHOW")
+    full_set$Common.Species.Name[i] = "Great Horned Owl"
+  else if(full_set$Species_Abv[i] == "OSPR")
+    full_set$Common.Species.Name[i] = "Osprey"
+  else if(full_set$Species_Abv[i] == "RSHA")
+    full_set$Common.Species.Name[i] = "Red-Shouldered Hawk"
+  else if(full_set$Species_Abv[i] == "COHA")
+    full_set$Common.Species.Name = "Cooper's Hawk"
+  else if(full_set$Species_Abv[i] == "MIKI")
+    full_set$Common.Species.Name[i] = "Mississippi Kite"
+  else if(full_set$Species_Abv[i] == "TUVU")
+    full_set$Common.Species.Name[i] = "Turkey Vulture"
+  else if(full_set$Species_Abv[i] == "BAEA")
+    full_set$Common.Species.Name[i] = "Bald Eagle"
+}
+
 
 # Make survival binary from "Disposition"
 Survive_full = c()
@@ -20,3 +60,5 @@ for(i in seq(1, nrow(full_set))){
 }
 full_set = cbind(full_set, Survive_full)
 Survive_full = full_set$Survive_full
+
+
